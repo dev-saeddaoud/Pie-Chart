f0a33823d609aa62a9045c09f13e63079cbef099
dev-saeddaoud <daoud.saed1@gmail.com>
2022-01-10 10:25:12 -0500

    added default duration and delay parameters to the tweened function, with the possibility of overridding them

--- src/helpers/createTween.ts
+++ src/helpers/createTween.ts
@@ -2,9 +2,9 @@ import { tweened } from 'svelte/motion';
 
 const createTweenValue = (
   value: number,
-  { duration } = { duration: 1000 },
-  { delay } = { delay: 0 }
+  defaults: { duration?: number; delay?: number } = {}
 ) => {
+  let { delay = 0, duration = 1000 } = Object.assign({}, defaults);
   return tweened(value, { duration: duration, delay: delay });
 };
 
