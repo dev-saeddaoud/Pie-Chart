d7d677e2b841d14bf6ceea53aea70f2d7f43de6c
dev-saeddaoud <daoud.saed1@gmail.com>
2022-01-13 20:46:26 -0500

    commit to change to a new branch for implementing transition on out

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -5,12 +5,12 @@
   export let entry: d3.PieArcDatum<AggregateExpenseItem>;
   export let radius: number;
 
+  export let prevEnd: number | undefined;
+
   let arcGenerator = d3
     .arc<d3.PieArcDatum<AggregateExpenseItem>>()
     .innerRadius(0)
     .outerRadius(radius);
-
-  export let prevEnd: number | undefined;
 
   let startAngleTween = createTweenValue(prevEnd ? prevEnd : 0);
   $: startAngleTween.set(+entry.startAngle);
@@ -32,6 +32,7 @@
     fill={entry.data.type === 'inc' ? 'var(--inc-color)' : 'var(--exp-color)'}
     stroke="black"
     stroke-width={2}
+    opacity="1"
   />
   <text
     text-anchor="middle"
